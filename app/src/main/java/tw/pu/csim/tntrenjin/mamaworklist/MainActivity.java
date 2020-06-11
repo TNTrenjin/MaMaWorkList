c997c84ec73f482d181935822a44bb95e609f2ab
TNTrenjin <45209009+TNTrenjin@users.noreply.github.com>
2020-06-11 19:23:06 +0800

    add some icon and fixed select work list page

--- app/src/main/java/tw/pu/csim/tntrenjin/mamaworklist/MainActivity.java
+++ app/src/main/java/tw/pu/csim/tntrenjin/mamaworklist/MainActivity.java
@@ -2,15 +2,30 @@ package tw.pu.csim.tntrenjin.mamaworklist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
+
+    Intent it;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        it = new Intent();
     }
+
+    public void selectWorkClick(View v) {
+        it.setClass(this, WorkListActivity.class);
+        startActivity(it);
+        finish();
+    }
+
+
 }
---
+++ app/src/main/java/tw/pu/csim/tntrenjin/mamaworklist/WorkListActivity.java
@@ -0,0 +1,16 @@
+package tw.pu.csim.tntrenjin.mamaworklist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class WorkListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_list);
+
+    }
+}
